37236d48660c2787212844dbc072d5f0b46ddf2d
Avanind Parjapati <avanind@pravaahconsulting.com>
2023-03-07 18:30:20 +0530

    Implement new stats for Admin Dashboard

--- src/pages/Admin Dashboard.v0esy.js
+++ src/pages/Admin Dashboard.v0esy.js
@@ -1,169 +1,219 @@
 // Velo API Reference: https://www.wix.com/velo/reference/api-overview/introduction
-import { currentMember } from 'wix-members';
-import { getRole } from 'backend/register.jsw';
-import wixData from 'wix-data';
-import wixLocation from 'wix-location';
-import { local } from 'wix-storage';
+import { currentMember } from "wix-members";
+import { getRole } from "backend/register.jsw";
+import wixData from "wix-data";
+import wixLocation from "wix-location";
+import { local } from "wix-storage";
 // let finalCount = [0, 0, 0]
-const ANIMATION_DELAY = [100, 100, 100]
+const ANIMATION_DELAY = [100, 100, 100];
 
 $w.onReady(function () {
+	// Bypassing role validation
+	const isValidRole = true; //getRoleValidation('Admin')
+	if (!isValidRole) {
+		console.log("Unauthorised Role trying to access Admin's page");
+	} else {
+		setAdminDashboardCounting();
+	}
+	restorePreviousState();
+	// Mapping Events to their respective handlers
+	$w("#createClassButton").onClick(createClassHandler);
+	$w("#searchClassButton").onClick(searchClassHandler);
+	$w("#updateClassButton").onClick(updateClassHandler);
+	$w("#deleteClassButton").onClick(deleteClassHandler);
+	$w("#enableDisableStudentButton").onClick(enableDisableStudentHandler);
+	$w("#enableDisableParentButton").onClick(enableDisableParentHandler);
+	$w("#enableDisableCoachButton").onClick(enableDisableCoachHandler);
+	$w("#approveCoachButton").onClick(approveCoachHandler);
 
-    // Bypassing role validation
-    const isValidRole = true //getRoleValidation('Admin')
-    if (!isValidRole) {
-        console.log('Unauthorised Role trying to access Admin\'s page')
-    } else {
-        setAdminDashboardCounting()
-    }
-    restorePreviousState();
-    // Mapping Events to their respective handlers
-    $w('#createClassButton').onClick(createClassHandler);
-    $w('#searchClassButton').onClick(searchClassHandler);
-    $w('#updateClassButton').onClick(updateClassHandler);
-    $w('#deleteClassButton').onClick(deleteClassHandler);
-    $w('#enableDisableStudentButton').onClick(enableDisableStudentHandler);
-    $w('#enableDisableParentButton').onClick(enableDisableParentHandler);
-    $w('#enableDisableCoachButton').onClick(enableDisableCoachHandler);
-    $w('#approveCoachButton').onClick(approveCoachHandler);
-
-
-    $w('#dashboardButton').onClick(() => {
-        $w('#multiStateBox1').changeState('dashboard');
-    })
-    $w('#manageClassButton').onClick(() => {
-        $w('#multiStateBox1').changeState('manageClass');
-    })
-    $w('#manageParentButton').onClick(() => {
-        $w('#multiStateBox1').changeState('manageParent');
-    })
-    $w('#manageCoachButton').onClick(() => {
-        $w('#multiStateBox1').changeState('manageCoach');
-    })
-    $w('#manageStudentButton').onClick(() => {
-        $w('#multiStateBox1').changeState('manageStudent');
-    })
-    $w('#multiStateBox1').onChange(() => {
-        saveCurrentStateToLocal();
-    })
+	$w("#dashboardButton").onClick(() => {
+		$w("#multiStateBox1").changeState("dashboard");
+	});
+	$w("#manageClassButton").onClick(() => {
+		$w("#multiStateBox1").changeState("manageClass");
+	});
+	$w("#manageParentButton").onClick(() => {
+		$w("#multiStateBox1").changeState("manageParent");
+	});
+	$w("#manageCoachButton").onClick(() => {
+		$w("#multiStateBox1").changeState("manageCoach");
+	});
+	$w("#manageStudentButton").onClick(() => {
+		$w("#multiStateBox1").changeState("manageStudent");
+	});
+	$w("#multiStateBox1").onChange(() => {
+		saveCurrentStateToLocal();
+	});
 });
 
 async function getRoleValidation(roleName) {
-    await currentMember.getRoles()
-        .then((roles) => {
-            const currentUserRole = getRole(roles);
-            const { error } = currentUserRole; // No need for this check, as it is handled by wix
-            if (error) {
-                console.log('Error in getting role : ', error)
-                return false
-            } else {
-                if (currentUserRole == roleName) return true
-                return false
-            }
-        })
-        .catch((error) => {
-            console.error(error);
-        });
+	await currentMember
+		.getRoles()
+		.then((roles) => {
+			const currentUserRole = getRole(roles);
+			const { error } = currentUserRole; // No need for this check, as it is handled by wix
+			if (error) {
+				console.log("Error in getting role : ", error);
+				return false;
+			} else {
+				if (currentUserRole == roleName) return true;
+				return false;
+			}
+		})
+		.catch((error) => {
+			console.error(error);
+		});
 }
 
 async function setAdminDashboardCounting() {
-    console.log('Done Till Here')
-    let parentCount = await wixData.query('Parents').find().then(res => res.items.length)
-    let studentCount = await wixData.query('Students').find().then(res => res.items.length)
-    let classCount = await wixData.query('Classes').find().then(res => res.items.length)
-    let finalCount = [studentCount, parentCount, classCount]
-    console.log('Done Till Here')
-    startAnimation(finalCount)
+	console.log("Done Till Here");
+	let parentCount = await wixData
+		.query("Parents")
+		.find()
+		.then((res) => res.items.length);
+	let studentCount = await wixData
+		.query("Students")
+		.find()
+		.then((res) => res.items.length);
+	let classCount = await wixData
+		.query("Classes")
+		.find()
+		.then((res) => res.items.length);
+	let finalCount = [studentCount, parentCount, classCount];
+	console.log("Done Till Here");
+	startAnimation(finalCount);
+	$w("#totalStudent").text = `Total student count: ${studentCount}`;
+	$w("#studentActive").text = `Total student active: ${await getActiveStudents()}`;
+	$w("#studentInactive").text = `Total student inctive: ${await getInactiveStudents(studentCount)}`;
+	$w("#studentPresent").text = `Total student inctive: ${await getPresentStudents()}`;
+	$w("#studentAbsent").text = `Total student inctive: ${await getAbsentStudents()}`;
 }
 
 function startAnimation(finalCount) {
-    let count1 = 0
-    let count2 = 0
-    let count3 = 0
+	let count1 = 0;
+	let count2 = 0;
+	let count3 = 0;
 
-    console.log('Final Count', finalCount)
+	console.log("Final Count", finalCount);
 
-    const countInterval1 = setInterval(changeCount1, ANIMATION_DELAY[0])
-    const countInterval2 = setInterval(changeCount2, ANIMATION_DELAY[1])
-    const countInterval3 = setInterval(changeCount3, ANIMATION_DELAY[2])
+	const countInterval1 = setInterval(changeCount1, ANIMATION_DELAY[0]);
+	const countInterval2 = setInterval(changeCount2, ANIMATION_DELAY[1]);
+	const countInterval3 = setInterval(changeCount3, ANIMATION_DELAY[2]);
 
-    function changeCount1() {
-        if (count1 <= finalCount[0]) {
-            $w('#studentCount').text = count1.toString();
-            count1 += 1
-        } else {
-            clearInterval(countInterval1)
-        }
-    }
+	function changeCount1() {
+		if (count1 <= finalCount[0]) {
+			$w("#studentCount").text = count1.toString();
+			count1 += 1;
+		} else {
+			clearInterval(countInterval1);
+		}
+	}
 
-    function changeCount2() {
-        if (count2 <= finalCount[1]) {
-            $w('#parentCount').text = count2.toString();
-            count2 += 1
-        } else {
-            clearInterval(countInterval2)
-        }
-    }
+	function changeCount2() {
+		if (count2 <= finalCount[1]) {
+			$w("#parentCount").text = count2.toString();
+			count2 += 1;
+		} else {
+			clearInterval(countInterval2);
+		}
+	}
 
-    function changeCount3() {
-        if (count3 <= finalCount[2]) {
-            $w('#classCount').text = count3.toString();
-            count3 += 1
-        } else {
-            clearInterval(countInterval3)
-        }
-    }
+	function changeCount3() {
+		if (count3 <= finalCount[2]) {
+			$w("#classCount").text = count3.toString();
+			count3 += 1;
+		} else {
+			clearInterval(countInterval3);
+		}
+	}
 }
 
 // Event Handlers
 
-const approveCoachHandler = ()=>{
-    wixLocation.to('/approve-coach')
-}
+const approveCoachHandler = () => {
+	wixLocation.to("/approve-coach");
+};
 
-const enableDisableStudentHandler = ()=>{
-    wixLocation.to('/enable-disable-school');
-}
-const enableDisableParentHandler = ()=>{
-    wixLocation.to('/enable-disable-parent');
-}
-const enableDisableCoachHandler = ()=>{
-    wixLocation.to('/enable-disable-coach');
-}
+const enableDisableStudentHandler = () => {
+	wixLocation.to("/enable-disable-school");
+};
+const enableDisableParentHandler = () => {
+	wixLocation.to("/enable-disable-parent");
+};
+const enableDisableCoachHandler = () => {
+	wixLocation.to("/enable-disable-coach");
+};
 const createClassHandler = () => {
-    // const pageUrl = "https://pravaah.editorx.io/emhsports/class-form?mode=modeValue&classId=classValue
-    //  modeValue can be of 1. read , 2. create , 3. edit
-    wixLocation.to('/class-form?mode=create');
-}
+	// const pageUrl = "https://pravaah.editorx.io/emhsports/class-form?mode=modeValue&classId=classValue
+	//  modeValue can be of 1. read , 2. create , 3. edit
+	wixLocation.to("/class-form?mode=create");
+};
 
 const searchClassHandler = () => {
-    // const pageUrl = "https://pravaah.editorx.io/emhsports/class-form?mode=modeValue&classId=classValue
-    //  modeValue can be of 1. read , 2. create , 3. edit
-    wixLocation.to('/admin-class-search');
-}
+	// const pageUrl = "https://pravaah.editorx.io/emhsports/class-form?mode=modeValue&classId=classValue
+	//  modeValue can be of 1. read , 2. create , 3. edit
+	wixLocation.to("/admin-class-search");
+};
 
 const updateClassHandler = () => {
-    // const pageUrl = "https://pravaah.editorx.io/emhsports/class-form?mode=modeValue&classId=classValue
-    //  modeValue can be of 1. read , 2. create , 3. edit
-    wixLocation.to('/admin-class-search');
-}
+	// const pageUrl = "https://pravaah.editorx.io/emhsports/class-form?mode=modeValue&classId=classValue
+	//  modeValue can be of 1. read , 2. create , 3. edit
+	wixLocation.to("/admin-class-search");
+};
 
 const deleteClassHandler = () => {
-    // const pageUrl = "https://pravaah.editorx.io/emhsports/class-form?mode=modeValue&classId=classValue
-    //  modeValue can be of 1. read , 2. create , 3. edit
-    wixLocation.to('/admin-class-search');
-}
+	// const pageUrl = "https://pravaah.editorx.io/emhsports/class-form?mode=modeValue&classId=classValue
+	//  modeValue can be of 1. read , 2. create , 3. edit
+	wixLocation.to("/admin-class-search");
+};
 
 // Custom Function
 const saveCurrentStateToLocal = () => {
-    const currentStateId = $w('#multiStateBox1').currentState.id;
-    local.setItem("adminDashboardPreviousState", currentStateId)
-}
+	const currentStateId = $w("#multiStateBox1").currentState.id;
+	local.setItem("adminDashboardPreviousState", currentStateId);
+};
 
 const restorePreviousState = () => {
-    const previousState = local.getItem('adminDashboardPreviousState');
-    if (!previousState) return;
-    console.log({ previousState });
-    $w('#multiStateBox1').changeState(previousState);
-    local.removeItem('adminDashboardPreviousState');
-}
+	const previousState = local.getItem("adminDashboardPreviousState");
+	if (!previousState) return;
+	console.log({ previousState });
+	$w("#multiStateBox1").changeState(previousState);
+	local.removeItem("adminDashboardPreviousState");
+};
+
+const getActiveStudents = async () => {
+	let res = null;
+	try {
+		res = await wixData.query("Carts").eq("status", "Paid").distinct("student");
+	} catch (error) {
+		console.log(error);
+	}
+    return res.totalCount;
+};
+const getInactiveStudents = async (totalStudent) => {
+    let res = null;
+	try {
+		res = await wixData.query("Carts").distinct("student");
+	} catch (error) {
+		console.log(error);
+	}
+    return (totalStudent - res.totalCount);
+};
+const getAbsentStudents = async () => {
+    let res = null;
+	try {
+		
+	} catch (error) {
+		console.log(error);
+	}
+    return "U/D";
+};
+const getPresentStudents = async () => {
+    let res = null;
+	try {
+		
+	} catch (error) {
+		console.log(error);
+	}
+    return 'U/D';
+};
